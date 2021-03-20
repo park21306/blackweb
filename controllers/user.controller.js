8ef852bbca6ac92cd5ce0c3c5dc880cdc6072728
GummyKitty <poonyanunt@gmail.com>
2021-03-20 12:22:39 +0700

    GummyKitty

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -4,6 +4,7 @@ import fetch from "node-fetch";
  * @param {import("express").Request} req 
  * @param {import("express").Response} res 
  */
+let fuckyou = []
 export const Login = (req, res) =>{
     // const {username,password} = req.query
     // const {username,password} =req.params
@@ -15,15 +16,36 @@ export const Login = (req, res) =>{
    
 }
 export const Users = async (req, res) =>{
-    const data = await fetch('https://jsonplaceholder.typicode.com/users')
-    const json = await data.json()
-    const { id } = req.params
-    const val    = json.filter((user)=> user.id === parseInt(id ))
-    res.json({
-        msg : 'users',
-        data : val,
+    // const data = await fetch('https://jsonplaceholder.typicode.com/users')
+    // const json = await data.json()
+    // const { id } = req.params
+    // const val    = json.filter((user)=> user.id === parseInt(id ))
+    // res.json({
+    //     msg : 'users',
+    //     data : val,
+    // })
+    res.json({ 
+        data : fuckyou,
+        
     })
 
 }
+export const R = async (req, res) =>{
+    const { name } = req.body
+    
+    fuckyou.push({name})
+    
+    res.json({
+        name,
+        
+    })
+}
+export const S =  (req, res) =>{
+    const { namei } = req.params
+    const v = fuckyou.filter((a)=>a.name==namei)
+    res.json({
+        data : v,
+    })
+}
 
-export default {Login,Users}
+export default {Login,Users,R,S}
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,9 +1,11 @@
 import express from "express";
-import {Login,Users} from '../controllers/user.controller.js'
+import {Login,R,S,Users} from '../controllers/user.controller.js'
 const user_route = express.Router()
 
 
 user_route.post('/login',(req,res) => Login(req,res))
-user_route.post('/:id',(req,res) => Users(req,res))
-
+// user_route.post('/:id',(req,res) => Users(req,res))
+user_route.get('/r',(req,res) => R(req,res))
+user_route.get('/s/:namei',(req,res) => S(req,res))
+user_route.get('/',(req,res) => Users(req,res))
 export default user_route
